09dd173d3da1e57862c6604787c5682475c1a575
zainqaimi <138384994+zainqaimi@users.noreply.github.com>
2025-02-01 19:12:18 +0500

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({
-    message: "User registered successfully",
+    message: "zain ul abdin",
   });
 });
 
