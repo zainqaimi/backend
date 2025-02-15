de3e0fa6029675208bfb55ac3939612e1039b5cb
zainqaimi <138384994+zainqaimi@users.noreply.github.com>
2025-02-16 00:22:57 +0500

    Update subscription.model.js

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,5 +1,19 @@
 import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchema = new Schema({});
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // wo jo subscribe kre ga
+      ref: "User",
+      // required: true,
+    },
+    channel: {
+      type: Schema.Types.ObjectId, // channel ke id
+      ref: "Channel",
+      // required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
