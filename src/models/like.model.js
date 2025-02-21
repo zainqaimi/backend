a0a40ebc6bef5df9700db63e8dbea511f591ee38
zainqaimi <138384994+zainqaimi@users.noreply.github.com>
2025-02-21 13:20:08 +0500

    create some models

---
+++ src/models/like.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new Schema(
+  {
+    video: {
+      type: Schema.Types.ObjectId, // wo jo liked kre ga
+      ref: "Video",
+    },
+    comment: {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    tweet: {
+      type: Schema.Types.ObjectId,
+      ref: "Tweet",
+    },
+    likedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Like = mongoose.model("Like", likeSchema);
